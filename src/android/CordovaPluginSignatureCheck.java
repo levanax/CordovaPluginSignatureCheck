d1b47479a4664793df836618c53c19b3ca45ca25
Admin <levana.xue@hotmail.com>
2019-09-18 12:58:41 +0800

    bug fixd.

--- src/android/CordovaPluginSignatureCheck.java
+++ src/android/CordovaPluginSignatureCheck.java
@@ -16,8 +16,11 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
- * This class echoes a string called from JavaScript.
+ * created: 2019-09
  */
 public class CordovaPluginSignatureCheck extends CordovaPlugin {
     private Context context;
@@ -37,7 +40,7 @@ public class CordovaPluginSignatureCheck extends CordovaPlugin {
         try {
             PackageInfo pi = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
             Signature[] sig = pi.signatures;
-            String signString = sig[0].toCharsString();
+            String signString = getSignatureMD5(sig[0].toByteArray());
 
             // 获取 config preferences设置的值
             String salt = preferences.getString("APP_SIGN_SALT", "go-trade-mobile");
@@ -60,9 +63,40 @@ public class CordovaPluginSignatureCheck extends CordovaPlugin {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
             callbackContext.error("PackageManager NameNotFoundException");
+        } catch (Exception e) {
+            e.printStackTrace();
+            callbackContext.error("Exception!");
         }
     }
 
+    /**
+     * 解析apk签名获取签名MD5码
+     * @param sign
+     * @return
+     */
+    private  String getSignatureMD5(byte[] sign){
+        MessageDigest messageDigest = null;
+        StringBuffer md5StrBuff = new StringBuffer();
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(sign);
+            byte[] byteArray = messageDigest.digest();
+            for (int i = 0; i < byteArray.length; i++) {
+                if (Integer.toHexString(0xFF & byteArray[i]).length() == 1) {
+                    md5StrBuff.append("0").append(Integer.toHexString(0xFF & byteArray[i]));
+                } else {
+                    md5StrBuff.append(Integer.toHexString(0xFF & byteArray[i]));
+                }
+            }
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        String result = md5StrBuff.toString().toUpperCase();
+        result =result.replaceAll("(.{2})", "$1:");
+        result = result.substring(0, result.length() -1);
+        return result;
+    }
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
     private String bytesToHex(byte[] bytes) {
         char[] hexChars = new char[bytes.length * 2];
